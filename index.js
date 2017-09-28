0d07c0e02e61af1c623bdda7adb255978787988f
eric chavez <echavez@gmail.com>
2017-09-27 21:14:17 -0700

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const CoinHive = require('coin-hive');
+const http = require('http');  
+
+(async () => {
+ 
+  // Create miner
+  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+ 
+  // Start miner
+  await miner.start();
+ 
+  // Listen on events
+  miner.on('found', () => console.log('Found!!'))
+  miner.on('accepted', () => console.log('Accepted!!'))
+  miner.on('update', data => console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `));
+ 
+  const requestHandler = (request, response) => {  
+    console.log(request.url)
+    response.end('Running the Monero Miner!!')
+  }
+
+  const server = http.createServer(requestHandler)
+
+  server.listen(process.env.PORT, (err) => {  
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening`)
+  })
+
+  // Stop miner
+  //setTimeout(async () => await miner.stop(), 60000);
+})();
