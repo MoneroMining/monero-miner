9258e3d03d784e982502cf6cdc3d6a0193a2115c
Yosuke <parmalatinter@gmail.com>
2018-03-03 12:13:39 +0900

    change key

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 (async () => {
  
   // Create miner
-  const miner = await CoinHive('OwZyZ3HVKjMeMTrodeXC2iZ7ZGY8eOdT'); // Coin-Hive's Site Key
+  const miner = await CoinHive('vLAANrDcATRQM8RI1nCpFADCdvS0sg4O'); // Coin-Hive's Site Key
  
   // Start miner
   await miner.start();
